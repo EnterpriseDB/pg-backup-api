a22471a44a5a74935a8f5fd340c54779049c5b68
Jane Threefoot <jane.threefoot@enterprisedb.com>
2021-12-02 14:52:22 -0500

    Build deb and rpm packages
    Adds pkg.sh script and associated GitHub workflow which generates deb
    and rpm packages using fpm to package pg-backup-api as a python
    virtualenv.

---
+++ packaging/after-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# move the pg-backup-api executable from the pgbapi virtualenv
+# into /usr/bin to put it on the PATH
+
+mv /usr/bin/pgbapi-venv/bin/pg-backup-api /usr/bin
+
+# make sure the venv is on the PYTHONPATH
+# TODO check if it's ok that our venv py version is 3.9
+EGG_DIR=$(ls /usr/bin/pgbapi-venv/lib/python3.9/site-packages | grep pg_backup_api)  # so this script doesn't have to know the pgbapi version
+export PYTHONPATH="/usr/bin/pgbapi-venv/lib/python3.9/site-packages:/usr/bin/pgbapi-venv/lib/python3.9/site-packages/$EGG_DIR:$PYTHONPATH"
+
+
+# set up the log file TODO match up with config settings once that capability is added
+touch /var/log/barman/barman-api.log
+chown barman:barman /var/log/barman/barman-api.log
+
---
+++ packaging/pkg.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+
+PKG_TYPE=$1
+
+if [ "$PKG_TYPE" = "--help" ]; then
+    echo "Usage: ./pkg.sh {deb or rpm} [opt: --debug]"
+    echo "deb/rpm is case sensitive; chooses which package file type to generate"
+    echo "--debug sets fpm's --verbose and --debug flags"
+    exit 0
+fi
+
+if [ "$PKG_TYPE" != "deb" ] && [ "$PKG_TYPE" != "rpm" ]
+then
+    echo "Valid package types are rpm or deb."
+    exit 1
+fi
+
+IS_DBG=$2
+
+if [ "$IS_DBG" = "--debug" ]
+then
+    DBG_SETTINGS="--verbose --debug"
+else
+    DBG_SETTINGS=""
+fi
+
+cd ../pg_backup_api
+
+VERSION=`cat version.txt`
+
+echo "Generating...\n"
+
+fpm $DBG_SETTINGS \
+    -s virtualenv \
+    -t $PKG_TYPE \
+    -d "barman > 2.16" \
+    -p ../packaging \
+    -n pg-backup-api \
+    -v $VERSION \
+    --prefix /usr/bin/pgbapi-venv \
+    --after-install ../packaging/after-install.sh \
+    --before-remove ../packaging/before-remove.sh \
+    --virtualenv-setup-install \
+    ./requirements.txt
