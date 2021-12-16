4227e71fd49265504195cfb14a84ce641ee2f06f
Jane Threefoot <jane.threefoot@enterprisedb.com>
2021-12-16 01:58:01 -0500

    add help text

--- pg_backup_api/pkg.sh
+++ pg_backup_api/pkg.sh
@@ -3,6 +3,13 @@
 
 PKG_TYPE=$1
 
+if [ "$PKG_TYPE" = "--help" ]; then
+    echo "Usage: ./pkg.sh {deb or rpm} [opt: --debug]"
+    echo "deb/rpm is case sensitive; chooses which package file type to generate"
+    echo "--debug sets fpm's --verbose and --debug flags"
+    exit 0
+fi
+
 if [ "$PKG_TYPE" != "deb" ] && [ "$PKG_TYPE" != "rpm" ]
 then
     echo "Valid package types are rpm or deb."
@@ -17,6 +24,8 @@ then
 else
     DBG_SETTINGS=""
 fi
+
+echo "Generating...\n"
 
 fpm $DBG_SETTINGS \
     -s virtualenv \
