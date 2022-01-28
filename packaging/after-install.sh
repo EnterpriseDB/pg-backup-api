41f645b8bd6dd8b7265e06d09eb7b0402979dac3
Jane Threefoot <jane.threefoot@enterprisedb.com>
2022-01-27 22:00:50 -0500

    set PYTHONPATH to make sure the venv code is findable

--- packaging/after-install.sh
+++ packaging/after-install.sh
@@ -5,3 +5,8 @@
 
 mv /usr/bin/pgbapi-venv/bin/pg-backup-api /usr/bin
 
+# make sure the venv is on the PYTHONPATH
+# TODO check if it's ok that our venv py version is 3.9
+EGG_DIR=$(ls /usr/bin/pgbapi-venv/lib/python3.9/site-packages | grep pg_backup_api)  # so this script doesn't have to know the pgbapi version
+export PYTHONPATH="/usr/bin/pgbapi-venv/lib/python3.9/site-packages:/usr/bin/pgbapi-venv/lib/python3.9/site-packages/$EGG_DIR:$PYTHONPATH"
+
