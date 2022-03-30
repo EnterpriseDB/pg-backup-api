3ba2e2bb9b204025d6d5c445c1b3a0747cedbc30
Michael Wallace <michael.wallace@enterprisedb.com>
2022-03-30 10:11:14 +0100

    Update copyright year

--- packaging/changelogs.py
+++ packaging/changelogs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-#  © Copyright EnterpriseDB UK Limited 2020-2021 - All rights reserved.
+#  © Copyright EnterpriseDB UK Limited 2021-2022 - All rights reserved.
 #
 """
 Generate changelogs for deb, rpm and humans from a source yaml file using
