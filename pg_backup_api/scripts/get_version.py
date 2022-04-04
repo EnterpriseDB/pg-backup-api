e9db1f0fa979943c9f3d938dadbe8da4171ba65e
Michael Wallace <michael.wallace@enterprisedb.com>
2022-04-04 12:33:27 +0100

    Add helper scripts for release process
    Adds a script to prepare the release and another script to read the
    version out of the news.yml file. These are referenced by the release
    process documented in the pg-backup-api-packaging repo.

---
+++ pg_backup_api/scripts/get_version.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+""" Determine the version string from news.yml. """
+
+import yaml
+
+path_to_news = "../pg_backup_api/news.yml"
+
+with open(path_to_news, "r") as fh:
+    news = yaml.safe_load(fh)
+
+latest_release = news["pg-backup-api release notes"][0]
+version = latest_release["version"]
+
+print(f"{version}")
